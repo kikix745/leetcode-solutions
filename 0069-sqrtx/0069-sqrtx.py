6f3e6391f54d3182bca487a2414d1ad7c37ebda3
OUNISSI ABDELDJALIL <157887766+kikix745@users.noreply.github.com>
2026-01-28 11:01:25 +0100

    Time: 0 ms (100%), Space: 12.5 MB (16.61%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        l=0
+        r=x
+        res=0
+        while l<=r:
+            m=(l+r)//2
+            if m**2>x:
+                r=m-1
+            elif m**2<x:
+                l=m+1
+                res=m
+            else :
+                return m    
+
+        return res                 
+        
